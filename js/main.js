693e8580830d123498b361fc2172ce09ebef3ea7
DreSean-Sutton <dreseansutton@gmail.com>
2022-02-14 15:48:47 -0800

    Now listening for submit event

--- js/main.js
+++ js/main.js
@@ -1,2 +1,30 @@
+/* eslint-disable no-unused-vars */
 /* global data */
 /* exported data */
+
+var $image = document.querySelector('#image');
+var $title = document.querySelector('#title');
+var $message = document.querySelector('#message');
+var $photoURL = document.querySelector('#photo-url');
+var $submit = document.querySelector('#submit');
+var $form = document.querySelector('#form');
+
+$photoURL.addEventListener('input', handleImageSwap);
+$form.addEventListener('submit', handleFormSubmit);
+
+function handleImageSwap(event) {
+  $image.src = event.target.value;
+}
+
+function handleFormSubmit(event) {
+
+  var formValues = {
+    title: $title.value,
+    photoURL: $photoURL.value,
+    message: $message.value,
+    nextEntryId: data.nextEntryId + 1
+  };
+  data.nextEntryId++;
+  data.entries.unshift(formValues);
+  $form.reset();
+}
