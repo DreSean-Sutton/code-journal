4f6b26cccccd716fdb58490eb151acf6a97c9e27
DreSean-Sutton <dreseansutton@gmail.com>
2022-02-14 17:52:32 -0800

    Defined a function that takes a single journal entry object and returns a DOM tree that matches one of the example entries in the HTML.

--- js/main.js
+++ js/main.js
@@ -30,4 +30,35 @@ function handleFormSubmit(event) {
   $image.src = 'images/placeholder-image-square.jpg';
 }
 
-// function
+/*
+-call the create element method of document with DIV as it's argument and assign it's value to var imgDiv
+-call the create element method of document with IMG as it's argument and assign it's value to var img
+-call the create element method of document with DIV as it's argument and assign it's value to var textDiv
+-call the create element method of document with h3 as it's argument and assign it's value to var entryTitle
+-call the create element method of document with p as it's argument and assign it's value to var entryText
+
+*/
+
+function renderEntries(entry) {
+  var $entryRow = document.createElement('ROW');
+  var $imgDiv = document.createElement('DIV');
+  var $entryImg = document.createElement('IMG');
+  var $textDiv = document.createElement('DIV');
+  var $entryTitle = document.createElement('H3');
+  var $entryText = document.createElement('P');
+
+  $entryRow.className = 'row';
+  $imgDiv.className = 'column-half';
+  $textDiv.className = 'column-half';
+
+  $entryImg.setAttribute('src', entry.photoURL);
+  $entryTitle.textContent = entry.title;
+  $entryText.textContent = entry.message;
+
+  $entryRow.appendChild($textDiv);
+  $entryRow.appendChild($imgDiv);
+  $imgDiv.appendChild($entryImg);
+  $textDiv.appendChild($entryText);
+  $textDiv.appendChild($entryTitle);
+  return $entryRow;
+}
